5771972a9bdaa5ca7f4710622bb26c9823480fba
uzayilica <uzayilica2@gmail.com>
2024-11-21 09:27:14 +0300

    first commit

---
+++ src/main/java/com/uzay/securitygeneltekrarr/SecurityGenelTekrarrApplication.java
@@ -0,0 +1,13 @@
+package com.uzay.securitygeneltekrarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGenelTekrarrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGenelTekrarrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uzay/securitygeneltekrarr/SecurityGenelTekrarrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzay.securitygeneltekrarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGenelTekrarrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
